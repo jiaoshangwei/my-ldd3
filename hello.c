f9ae49dd5f10a3aacc386bcbbda5f781aea44ff1
jiaoshangwei <jsw_4@163.com>
2014-08-11 05:24:38 -0400

    ldd3 1:add linux/sched.h

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
 #include <linux/moduleparam.h>
 #include <linux/version.h>
 #include <linux/errno.h>
-
+#include <linux/sched.h>
 
 static int j_int = 0;
 static char *j_charp = "NULL";
@@ -18,15 +18,20 @@ MODULE_LICENSE("GPL");
 
 static int __init hello_init(void)
 {
+    uint temp = KERNEL_VERSION(3,10,0);
+    struct task_struct *t = current;
     printk(KERN_INFO "hello_init() j_int=%d, j_charp=%s",j_int,j_charp);
     printk(KERN_INFO "hello_init() LINUX_VERSION_CODE= %d",LINUX_VERSION_CODE);
-    uint temp = KERNEL_VERSION(3,10,0);
     printk(KERN_INFO "hello_init() KERNEL_VERSION=%d",temp);
+
+    printk(KERN_INFO "hello_init() t->pid=%d t->comm=%s", t->pid,t->comm);
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
+    struct task_struct *t = current; //current is the same as get_current()
+    printk(KERN_INFO "hello_exit() t->pid=%d t->comm=%s",t->pid, t->comm);
     printk(KERN_INFO "hello_exit() ");
     return;
 }
